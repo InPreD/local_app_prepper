0a973b40d3c09253e3c8600389137fcb3888e205
Martin Rippin <marrip@ihelse.net>
2023-11-23 14:34:55 +0000

    feat: produce inputs.json for demultiplexing, one per sample for analysis and one for gather

--- prepper/cli.py
+++ prepper/cli.py
@@ -20,29 +20,37 @@ novaseq = [
 @click.option('--gather-input-json-template', help='path to template input.json for gather workflow', default=os.path.join(os.path.dirname(sys.argv[0]), 'templates/gather.json'), type=click.Path(exists=True))
 @click.option('--input', '-i', help='sequencing run output folder path', required=True, type=click.Path(exists=True))
 @click.option('--run-info-xml', help='RunInfo.xml file name', default='RunInfo.xml', type=str)
+@click.option('--samples', '-s', help='comma-separated list of sample ids', required=True, type=str)
 @click.option('--tso500-input-json-template', help='path to template input.json for TSO500 workflow', default=os.path.join(os.path.dirname(sys.argv[0]), 'templates/tso500.json'), type=click.Path(exists=True))
-def cli(gather_input_json_template, input, run_info_xml, tso500_input_json_template):
+def cli(gather_input_json_template, input, run_info_xml, samples, tso500_input_json_template):
     # read json templates
     tso500 = get_json(tso500_input_json_template)
     gather = get_json(gather_input_json_template)
 
-    # get values
+    # set isNovaSeq var
     isNovaSeq = is_novaseq(os.path.join(input, run_info_xml))
-    startFromFastq, fastqFolder = detect_fastq(input)
-
-    # generate input.json for TSO500
     tso500['TSO500.isNovaSeq'] = isNovaSeq
-    tso500['TSO500.startFromFastq'] = startFromFastq
-    tso500['TSO500.fastqFolder'] = fastqFolder
-    write_json(tso500, 'tso500.json')
+    gather['GatherResultsWorkflow.isNovaSeq'] = isNovaSeq
+
+    # generate inputs.json for TSO500 demultiplexing
+    demultiplex = tso500
+    demultiplex['TSO500.demultiplex'] = True
+    write_json(demultiplex, 'demultiplex.json')
+
+    # generate inputs.json files for each sample for TSO500 DNA and RNA analysis
+    # commented out as we don't want an automatic check but rather have people specify input fastq files
+    for sample in samples.split(','):
+        tso500['TSO500.sampleOrPairIDs'] = sample
+        tso500['TSO500.startFromFastq'] = True
+        tso500['TSO500.fastqFolder'] = 'FASTQFOLDER'
+        write_json(tso500, ('tso500_%s.json' % sample))
 
     # generate input.json for gather
-    gather['GatherResultsWorkflow.isNovaSeq'] = isNovaSeq
-    gather['GatherResultsWorkflow.startFromFastq'] = startFromFastq
+    gather['GatherResultsWorkflow.startFromFastq'] = True
     write_json(gather, 'gather.json')
 
 """
-    use glob to find fastq and return folder containing them or empty string if not
+    use glob to find fastq and return folder containing them or empty string if not - currently not used anymore, TODO: remove
 """
 def detect_fastq(path):
     fastq_files = glob.glob(os.path.join(path, fastq_file_glob), recursive=True)
