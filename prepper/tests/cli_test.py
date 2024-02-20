1bd08c83f1f3011c2dafd85c8325531986d6dba0
Martin Rippin <marrip@ihelse.net>
2024-02-20 08:03:48 +0000

    tests: use parametrized pytest unit testing and check exceptions

--- prepper/tests/cli_test.py
+++ prepper/tests/cli_test.py
@@ -1,11 +1,29 @@
 import pytest
 import prepper.cli
 
-class TestIsNovaseq:
-    def test_novaseq(self):
-        output = prepper.cli.is_novaseq('test/230101_NDX123456_0001_ABCDEFGH/RunInfo.xml')
-        assert output is True
+from contextlib import nullcontext as does_not_raise
+from json import decoder
 
-    def test_no_novaseq(self):
-        output = prepper.cli.is_novaseq('test/230101_NDX123456_0002_ABCDEFGH/RunInfo.xml')
-        assert output is False
+@pytest.mark.parametrize(
+    "input, exception, want",
+    [
+        ('test/test.json', does_not_raise(), {'key': 'value'}),
+        ('test/test.yaml', pytest.raises(decoder.JSONDecodeError), {}),
+        ('test/non-existent.json', pytest.raises(FileNotFoundError), {}),
+    ]
+)
+def test_get_json(input, exception, want):
+    with exception:
+        assert prepper.cli.get_json(input) == want
+
+@pytest.mark.parametrize(
+    "input, exception, want",
+    [
+        ('test/230101_NDX123456_0001_ABCDEFGH/RunInfo.xml', does_not_raise(), True),
+        ('test/230101_NDX123456_0002_ABCDEFGH/RunInfo.xml', does_not_raise(), False),
+        ('test/230101_NDX123456_0003_ABCDEFGH/RunInfo.xml', pytest.raises(IndexError), False),
+    ]
+)
+def test_is_novaseq(input, exception, want):
+    with exception:
+        assert prepper.cli.is_novaseq(input) == want
